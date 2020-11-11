6381b505a4096d4a75dcee625f3b80e830148f2a
hristoyanov <73996482+hristoyanov@users.noreply.github.com>
2020-11-11 18:28:37 +0200

    Added/changed stuff
    Changed from url fields to file upload for images.

--- card_library/settings.py
+++ card_library/settings.py
@@ -125,3 +125,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     join(BASE_DIR, 'static'),
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = join(BASE_DIR, 'media')
--- main/models.py
+++ main/models.py
@@ -5,7 +5,10 @@ from django.db import models
 class ExpansionSet(models.Model):
     name = models.CharField(max_length=50, blank=False)
     card_count = models.PositiveIntegerField(blank=False)
-    image_url = models.URLField(blank=False)
+    image = models.ImageField(upload_to='card_set_images', blank=False)
+
+    def __str__(self):
+        return self.name
 
 
 class Card(models.Model):
@@ -35,7 +38,7 @@ class Card(models.Model):
     hero_class = models.CharField(max_length=12, blank=False, choices=hero_classes)
     mana_cost = models.PositiveIntegerField(blank=False)
     rarity = models.CharField(max_length=9, blank=False, choices=rarities)
-    image_url = models.URLField(blank=False)
+    image = models.ImageField(upload_to='card_images', blank=False)
     expansion_set = models.ForeignKey(ExpansionSet, on_delete=models.CASCADE)
 
     def __str__(self):
