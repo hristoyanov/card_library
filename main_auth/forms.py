33c0b450b3c53e7bbd2ccb3fcd43ec3abcce18ff
hristoyanov <73996482+hristoyanov@users.noreply.github.com>
2020-12-03 17:24:58 +0200

    Small changes.

--- main_auth/forms.py
+++ main_auth/forms.py
@@ -1,15 +1,13 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+
+from main_core.mixins import BootstrapFormMixin
 
 
-class RegisterForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ('username',)
-        widgets = {
-            'password': forms.PasswordInput(),
-        }
+class RegisterForm(UserCreationForm, BootstrapFormMixin):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setup_form()
 
 
 class LoginForm(forms.Form):
