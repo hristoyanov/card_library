2f017d90d47707ab6611d73b08e7c1489fa7d71d
hristoyanov <73996482+hristoyanov@users.noreply.github.com>
2020-12-10 17:39:20 +0200

    Changed admin model registration.
    Changed ExpansionSet, Card and CollectionCard models so they show more info in the admin page.

--- main/admin.py
+++ main/admin.py
@@ -5,8 +5,20 @@ from main.models.expansion_set import ExpansionSet
 from main.models.card import Card
 from main.models.collection_card import CollectionCard
 
-# Register your models here.
-admin.site.register(ExpansionSet)
-admin.site.register(Card)
-admin.site.register(CollectionCard)
+
+class ExpansionSetAdmin(admin.ModelAdmin):
+    list_display = ('name', 'card_count', 'release_date')
+
+
+class CardAdmin(admin.ModelAdmin):
+    list_display = ('name', 'expansion_set')
+
+
+class CollectionCardAdmin(admin.ModelAdmin):
+    list_display = ('card', 'copies', 'user')
+
+
+admin.site.register(ExpansionSet, ExpansionSetAdmin)
+admin.site.register(Card, CardAdmin)
+admin.site.register(CollectionCard, CollectionCardAdmin)
 admin.site.register(ClassPortrait)
